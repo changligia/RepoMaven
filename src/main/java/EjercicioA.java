6bff91357344857b7e86c91bc8f69c14f62c19c2
Ligia Chang <EXTLICR@mapfre.com.pe>
2022-06-11 17:35:44 -0500

    Agrego mas imprimir

--- src/main/java/EjercicioA.java
+++ src/main/java/EjercicioA.java
@@ -1,5 +1,10 @@
 public class EjercicioA {
     public static void main(String[] args) {
         System.out.println("Hola Profesor");
+        System.out.println("Hola Profesor");
+        System.out.println("Hola Profesor");
+        System.out.println("Hola Profesor");
+        System.out.println("Hola Profesor");
+        System.out.println("Hola Profesor");
     }
 }
