a9223eb488acb24cfc6e4d5124ebeccef2b3659c
Ligia Chang <EXTLICR@mapfre.com.pe>
2022-06-11 17:55:42 -0500

    corrigiendo lineas indicadas

--- src/main/java/EjercicioA.java
+++ src/main/java/EjercicioA.java
@@ -1,10 +1,10 @@
 public class EjercicioA {
     public static void main(String[] args) {
         System.out.println("Hola Profesor");
+        System.out.println("Hola alumno");
+        System.out.println("Hola Retador");
         System.out.println("Hola Profesor");
-        System.out.println("Hola Profesor");
-        System.out.println("Hola Profesor");
-        System.out.println("Hola Profesor");
+        System.out.println("Hola tutu");
         System.out.println("Hola Profesor");
     }
 }
